9cddffe9799f0d0eb7445a54d7a3cf7f493146a6
moretti-matteo <matteomoretti2000@hotmail.it>
2022-07-14 16:46:39 +0200

    creata migration, model e controller

---
+++ database/migrations/2022_07_14_134514_create_fumetti_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFumettiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fumetti', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('description', 500);
+            $table->string('thumb', 500);
+            $table->decimal('price');
+            $table->string('series', 40);
+            $table->date('sale_date');
+            $table->string('type', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fumetti');
+    }
+}
